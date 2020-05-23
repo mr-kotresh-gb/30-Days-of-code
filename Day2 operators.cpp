daf3748913b67eb04fbb43ad1c9a7aedbb6e680e
Kotresh G B <60075769+kotreshgb@users.noreply.github.com>
2020-05-23 11:45:14 +0530

    Create Day2 operators.cpp

---
+++ Day2 operators.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+    double tip=meal_cost*tip_percent/100;
+    double tax=meal_cost*tax_percent/100;
+    int totalCost=(int)round(meal_cost+tip+tax);    
+    cout<<totalCost;
+}
+
+int main()
+{
+    double meal_cost;
+    cin >> meal_cost;
+
+    int tip_percent;
+    cin >> tip_percent;
+
+    int tax_percent;
+    cin >> tax_percent;
+    solve(meal_cost, tip_percent, tax_percent);
+
+    return 0;
+}
