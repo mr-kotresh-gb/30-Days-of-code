8f949936b11380285f8914d80b2dd5f23870045e
Kotresh G B <60075769+kotreshgb@users.noreply.github.com>
2020-05-26 09:30:58 +0530

    Create Day5 Loops.cpp

---
+++ Day5 Loops.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    for(int i=1;i<=10;i++)
+    {
+        cout<<n<<" x "<<i<<" = "<< i*n <<endl;
+    }
+    return 0;
+}
