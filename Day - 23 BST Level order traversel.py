29243ee983de448403ac31a36c3f14bc48ac2de3
Kotresh G B <60075769+kotreshgb@users.noreply.github.com>
2020-06-21 14:42:53 +0530

    Create Day - 23 BST Level order traversel.py

---
+++ Day - 23 BST Level order traversel.py
@@ -0,0 +1,37 @@
+//Source - https://www.hackerrank.com/challenges/30-binary-trees/problem
+
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        q = [ root ]
+        for current in q:    
+            if current:
+                print(current.data, end=' ')
+                q.append(current.left)
+                q.append(current.right) 
+
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
