3f28dc9a74646f4ab153555e4d0c980a9ab0727c
Kotresh G B <60075769+kotreshgb@users.noreply.github.com>
2020-07-01 16:09:56 +0530

    Create Day-27 Testing.java

---
+++ Day-27 Testing.java
@@ -0,0 +1,17 @@
+//Source - https://www.hackerrank.com/challenges/30-testing/problem
+
+class Solution {
+    public static void main(String srgs[]) {
+        System.out.println(5);
+        System.out.println("4 3");
+        System.out.println("0 -3 4 2");
+        System.out.println("5 2");
+        System.out.println("0 -3 4 2 2");
+        System.out.println("3 3");
+        System.out.println("0 -3 4");
+        System.out.println("7 2");
+        System.out.println("0 -3 1 1 1 1 1");
+        System.out.println("6 3");
+        System.out.println("0 -3 4 2 1 1");
+    }
+}
